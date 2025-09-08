73d53c4b8e50f8d3f88eea5be3b5a272162fbb1e
RainySunsetGod <carl.mikaelsson@outlook.com>
2025-09-08 17:06:47 +0200

    test

--- adventure-evolution/src/data/enemies/index.ts
+++ adventure-evolution/src/data/enemies/index.ts
@@ -1,9 +1,11 @@
 import { SLIMES } from "./slimes";
 import { BEASTS } from "./beasts";
 import { BANDITS } from "./bandits";
+import { CULT_OF_THE_SCALE } from "./cult_of_the_scale";
 
 export const ENEMIES = [
   ...SLIMES,
   ...BEASTS,
   ...BANDITS,
+  ...CULT_OF_THE_SCALE,
 ];
